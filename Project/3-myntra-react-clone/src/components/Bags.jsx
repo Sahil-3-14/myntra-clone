edce16bacd48745c65da01db7b129bde86bd1f3c
Sahil <Sahilkumar9190@gmail.com>
2024-07-01 23:23:15 +0530

    started a new clone project using React

---
+++ Project/3-myntra-react-clone/src/components/Bags.jsx
@@ -0,0 +1,12 @@
+import Footer from "./Footer";
+import Header from "./Header";
+
+const Bag = () => {
+  <>
+    <Header />
+    <main>
+      <div className="items-container"></div>
+    </main>
+    <Footer />
+  </>;
+};
